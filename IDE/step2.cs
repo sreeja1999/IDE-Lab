413553ef614b0979baed705d58cf5395a54eb7c4
sreeja1999 <59191308+sreeja1999@users.noreply.github.com>
2020-09-03 16:32:10 +0530

    Add files via upload

---
+++ IDE/step2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            String name = "sreeja";
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
